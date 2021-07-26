b59672ff2a7a2be984092792c40b2e62975134e1
Sunku Sai Purnima <69891257+sunkusaipurnima@users.noreply.github.com>
2021-07-26 22:28:58 +0530

    Add files via upload

---
+++ Screens/SearchScreen.js
@@ -0,0 +1,18 @@
+/** @format */
+
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const SearchScreen = (props) => {
+  return (
+    <View style={styles.container}>
+      <Text>Search</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default SearchScreen;
